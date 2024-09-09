b42eb1504dd833ee52c0e54a521ec29fc04709d9
Hanakhin <hanakhin.nounimassotte@gmail.com>
2024-09-09 07:01:45 +0200

    fixed things

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -7,21 +7,21 @@ import {LinkedinIcon} from "@/app/_components/icons/LinkedinIcon";
 
 export const Header = () =>{
     return(
-        <header className={'sticky top-0 py-4'}>
+        <header className={' top-0 py-4'}>
             <Section className={'flex items-baseline'}>
                 <h1 className={'text-lg font-bold text-primary-foreground'}>Hanakhin.com</h1>
                 <div className={'flex-1'}/>
                 <ul className={'flex items-center gap-2'}>
                     <Link
                         href={'https://github.com/hanakhin'}
-                        className={cn(buttonVariants({variant: 'outline'}), 'size-10 p-2')}
+                        className={cn(buttonVariants({variant: 'outline'}), 'size-10 p-2 rounded')}
                         target={'_blank'}
                     >
                         <GithubIcon size={22} className={'text-foreground'}/>
                     </Link>
                     <Link
                         href={'https://linkedin.com/hanakhin'}
-                        className={cn(buttonVariants({variant: 'outline'}), 'size-10 p-2')}
+                        className={cn(buttonVariants({variant: 'outline'}), 'size-10 p-2 rounded')}
                         target={'_blank'}
                     >
                         <LinkedinIcon size={22} className={'text-foreground'}/>
